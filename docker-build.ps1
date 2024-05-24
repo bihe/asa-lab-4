5268e08afa3f0a1c823474ce840ce286b969a573
Henrik Binggl <henrik.binggl@gmail.com>
2024-05-24 14:07:48 +0200

    fix(powershell): add agent-python to the powershell scirpt

--- docker-build.ps1
+++ docker-build.ps1
@@ -1,3 +1,4 @@
 minikube docker-env --shell powershell | Invoke-Expression
 docker build -t dapr-demo/agent-golang ./agent-golang
+docker build -t dapr-demo/agent-python ./agent-python
 docker build -t dapr-demo/dashboard-dotnet ./dashboard-dotnet
--- k8s-undeploy.ps1
+++ k8s-undeploy.ps1
@@ -1,4 +1,5 @@
 kubectl delete deployment agent-golang
+kubectl delete deployment agent-python
 kubectl delete deployment dashboard-dotnet
 kubectl delete service dashboard-service
 kubectl delete component pubsub
